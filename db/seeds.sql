f552800e7c00cb21a686e221bcc8f004abe5ba9a
Alex Sanschagrin <asanscha@gmail.com>
2019-09-26 19:50:39 -0700

    created file structure for project

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+USE burgers;
+
+INSERT INTO burgers (burger_name)
+VALUES ("Bacon Cheese Burger");
+
+INSERT INTO burgers (burger_name)
+VALUES ("Veggie Burger");
+
+INSERT INTO burgers (burger_name)
+VALUES ("Turkey Burger");
